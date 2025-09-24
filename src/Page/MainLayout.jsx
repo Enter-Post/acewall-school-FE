63266f0b616cc3315cd951ba9cfa6d4d45e3d1a7
Talhah Asif <98359013+TalhahAsif@users.noreply.github.com>
2025-09-25 01:05:55 +0500

    Contact us featured page (#4)
    * working on the snding mai
    
    * landing page done

--- src/Page/MainLayout.jsx
+++ src/Page/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, Link, Outlet } from "react-router-dom";
+import { useNavigate, Link, Outlet, useLocation } from "react-router-dom";
 import { useContext } from "react";
 import { Button } from "@/components/ui/button";
 
@@ -8,10 +8,11 @@ import acewallshort from "../assets/acewallshort.png";
 import Footer from "@/CustomComponent/Footer";
 import ScrollToTop from "@/lib/scrolltop";
 import { GlobalContext } from "@/Context/GlobalProvider";
-  
+
 const MainLayout = () => {
   const navigate = useNavigate();
   const { user } = useContext(GlobalContext);
+  const location = useLocation();
 
   const handleGoToAccount = () => {
     if (!user) {
@@ -27,12 +28,19 @@ const MainLayout = () => {
     }
   };
 
+  // Show stripe only on FeaturedPage ("/")
+  const showBlueStripe = location.pathname === "/";
+
   return (
     <>
       <ScrollToTop />
       <div className="flex h-screen flex-col w-screen">
-        <div className="h-8 bg-green-600 flex justify-end items-end px-5 cursor-pointer" />
+        {/* Blue stripe only on FeaturedPage */}
+        {showBlueStripe && (
+          <div className="h-10 bg-[#156082] w-full"></div>
+        )}
         <header className="sticky top-0 z-10 bg-white w-full">
+          <div className="h-3 bg-green-600 flex justify-end items-end px-5 cursor-pointer" />
           <div className="flex h-16 items-center justify-between px-4">
             <Link className="block md:hidden" to={"/"}>
               <img src={acewallshort} alt="Mobile Logo" className="w-8 rounded-full h-auto cursor-pointer" />
