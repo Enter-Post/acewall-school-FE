35212e37fa1df1fba2f7d4159dc46d99bef7c816
talhahasif <imtalha995@gmail.com>
2025-10-08 20:56:26 +0500

    dsf

--- src/Page/teacher/Courses/CoursesBasics.jsx
+++ src/Page/teacher/Courses/CoursesBasics.jsx
@@ -364,7 +364,7 @@ export default function CoursesBasis() {
                 />
               </div>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <SelectSemevster
+                <SelectSemester
                   register={register}
                   errors={errors}
                   selectedSemester={selectedSemester}
